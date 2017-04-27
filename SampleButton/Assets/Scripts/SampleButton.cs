a2a14a98f827cfbd2b588e3c48a157f3a7fa6f67
chen <cy_chance@hotmail.com>
2017-04-27 14:32:59 +0800

    添加scroll view实例

---
+++ SampleButton/Assets/Scripts/SampleButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleButton : MonoBehaviour {
+
+    public Button button;
+    public Text itemName;
+    public Text itemPrict;
+    public Image itemIcon;
+
+    private Item item;
+    private ShopScrollList scrollList;
+
+    // Use this for initialization
+    void Start () {
+        button.onClick.AddListener(OnClick);
+    }
+
+    public void Setup(Item currentItem, ShopScrollList list) {
+
+        item = currentItem;
+        scrollList = list;
+
+        itemName.text = item.itemName;
+        itemPrict.text = item.price.ToString();
+        itemIcon.sprite = item.icon;
+    }
+
+    public void OnClick()  {
+
+        scrollList.TryTransferItemToOtherShop(item);
+    }
+}

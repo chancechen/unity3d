77fa267fb2908b7d3f203e951ea6993922b751e0
chen <cy_chance@hotmail.com>
2017-05-03 10:37:19 +0800

    修改服务器状态显示，添加学习日志记录

--- SampleButton/Assets/Scripts/ServerManager.cs
+++ SampleButton/Assets/Scripts/ServerManager.cs
@@ -1,14 +1,23 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
+//[System.Serializable]
+public enum sstatus
+{
+    General= 0xAAAA00,
+    Busy = 0xAA0000,
+    Free = 0x00AA00,
+}
 
 [System.Serializable]
 public class ServerInfo
 {
     public string name;
     public string ip;
-    public uint port;    
+    public uint port;
+    public sstatus status;
 }
 
 public class ServerManager : MonoBehaviour
@@ -39,6 +48,17 @@ public class ServerManager : MonoBehaviour
         }
     }
 
+    static Color ConvertHexToColor( uint c)
+    {  
+        Color32 color;
+        color.r = (byte)(c >> 16 & 0xff);
+        color.g = (byte)(c >> 8 & 0xff);
+        color.b = (byte)(c >> 0 & 0xff);
+        color.a = 0xff;
+
+        return color;
+    }
+
     private void AddServers()
     {
 
@@ -54,7 +74,7 @@ public class ServerManager : MonoBehaviour
             newButton.transform.localScale = Vector3.one;
             newButton.transform.localPosition = Vector3.zero;
             newButton.transform.localRotation = Quaternion.AngleAxis(0f, new Vector3(0f, 15f, 0f));
-
+            
             ServerItem sampleButton = newButton.GetComponent<ServerItem>();
             sampleButton.Setup(item, this);
 
@@ -63,6 +83,12 @@ public class ServerManager : MonoBehaviour
                 currentServer = item;
                 showServer.text = item.name;
             }
+            
+            var st= sampleButton.transform.Find("Status").gameObject;
+            if (st != null)
+            {
+                st.GetComponent<Image>().color = ConvertHexToColor((uint)item.status);
+            }            
         }
     }
 

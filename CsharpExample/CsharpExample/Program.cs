506849f6707d46dc778ce8c1b9d30bb95cf1bac4
chen <cy_chance@hotmail.com>
2017-07-21 09:25:39 +0800

    添加unity游戏资源路径

--- CsharpExample/CsharpExample/Program.cs
+++ CsharpExample/CsharpExample/Program.cs
@@ -11,26 +11,53 @@ namespace CsharpExample
         
         static void Main(string[] args)
         {
-            ArgsExample.ForeachShowArgs(args);
-            ArgsExample.ForShowArgs(args);
+            //ArgsExample.ForeachShowArgs(args);
+            //ArgsExample.ForShowArgs(args);
 
-            ArrayExgample.ForeachShowArray2();
+            //ArrayExgample.ForeachShowArray2();
 
-            PropertyExample.GetInstance().Age = 100;
-            PropertyExample.GetInstance().Name = "Chen Chaozhong";
-            PropertyExample.GetInstance().Show();
+            //PropertyExample.GetInstance().Age = 100;
+            //PropertyExample.GetInstance().Name = "Chen Chaozhong";
+            //PropertyExample.GetInstance().Show();
+            //ClassVersion.GetInstance().Show();
+            //ForeachClass.GetInstance().Show();
+            //StrcutExample.GetInstance().Show();
+            //Indexer.GetInstance().Show();
+            //IndexerProperty.GetInstance().Show();
+            //RomanNumerial.GetInstance().Show();
+            //OperatorExample.GetInstance().Show();
+            //DelegateExample.GetInstance().Show();
+            //EventExample.GetInstance().Show();
+            //InterfaceExample.GetInstance().Show();
+            //ConditionalExample.GetInstance().Show();
 
-            ClassVersion.GetInstance().Show();
-            ForeachClass.GetInstance().Show();
-            StrcutExample.GetInstance().Show();
-            Indexer.GetInstance().Show();
-            IndexerProperty.GetInstance().Show();
-            RomanNumerial.GetInstance().Show();
-            OperatorExample.GetInstance().Show();
-            DelegateExample.GetInstance().Show();
-            EventExample.GetInstance().Show();
-            InterfaceExample.GetInstance().Show();
-            ConditionalExample.GetInstance().Show();
+            Dictionary<int, int> map = new Dictionary<int, int>()
+            {
+                {10,10 },
+                {50,10 },
+                {70,10 },
+                {40,10 },
+                {20,10 },
+                {60,10 },
+                {30,10 }
+            };
+
+            List<KeyValuePair<int, int>> list=new List<KeyValuePair<int, int>>(map);
+            list.Sort((a,b)=> {
+                return a.Key > b.Key?1:-1;
+            });
+
+
+            foreach (KeyValuePair<int,int> it in list)
+            {
+                Console.WriteLine(it.Key);
+            }
+            List<int> alist = new List<int>() { 1,9,8,6,7,4,2,3};
+            alist.Sort((a,b)=> { return a < b?1:-1; });
+            foreach (int it in alist)
+            {
+                Console.WriteLine(it);
+            }
         }
     }
 }
